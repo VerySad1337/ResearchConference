d19316553c7be3fe31056b358889dcb82ad01bae
yeechaotee <yechaotee1995@gmail.com>
2022-11-10 15:27:47 +0800

    pull request only

--- Research Conference/ResearchConference/AddComments.aspx.cs
+++ Research Conference/ResearchConference/AddComments.aspx.cs
@@ -13,58 +13,6 @@ namespace ResearchConference
     public partial class AddComments : System.Web.UI.Page
     {
         SqlConnection dbConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["RCMSConnectionString"].ConnectionString);
-        class addCommentsController
-        {
-            SqlConnection dbConnections = new SqlConnection(ConfigurationManager.ConnectionStrings["RCMSConnectionString"].ConnectionString);
-            
-            public SqlCommand displayCurrentPaperTitle (string currentSessionPaperID)
-            {
-                dbConnections.Open();
-                addCommentEntity displayCurrentPaperTitle = new addCommentEntity();
-                string sessionPaperID = currentSessionPaperID;
-                dbConnections.Close();
-                return displayCurrentPaperTitle.getPaperTitle(sessionPaperID);
-            }
-
-            public void storeComments(string comments, string currentSessionPaperID, string currentSessionUserID)
-            {
-                string PaperID = currentSessionPaperID;
-                string inputComments = comments;
-                string UserID = currentSessionUserID;
-                addCommentEntity addNewComment = new addCommentEntity();
-                addNewComment.setComments(inputComments,PaperID,UserID);
-            }
-
-
-        }
-
-        class addCommentEntity
-        {
-            SqlConnection dbConnections = new SqlConnection(ConfigurationManager.ConnectionStrings["RCMSConnectionString"].ConnectionString);
-            public SqlCommand getPaperTitle(string getPaperID)
-            {
-                dbConnections.Open();
-                string currentPaperSessionID = getPaperID;
-                addCommentEntity myEntity = new addCommentEntity(); //Dont Make any sense if u mark, but you require BCE!.             
-                SqlCommand SQLQuery = new SqlCommand("Select Paper.PaperTitle from Paper INNER JOIN Allocation On Allocation.PaperID = Paper.PaperID where Paper.PaperID = " + currentPaperSessionID, dbConnections);
-                return SQLQuery;
-            }
-
-            public void setComments(string comments, string currentSessionPaperID, string currentSessionUserID)
-            {
-                dbConnections.Open();
-                string PaperID = currentSessionPaperID;
-                string inputComments = comments;
-                string UserID = currentSessionUserID;
-                DateTime currentTime = DateTime.Now;
-                SqlCommand insertCommand = dbConnections.CreateCommand();
-                insertCommand.CommandType = CommandType.Text;
-                insertCommand.CommandText = "Insert into Comments(Comments,PaperID,CreatedDate,UserID ) values('" +inputComments + "' , '" + PaperID + "', '" + currentTime + "', '" + UserID + "')";
-                insertCommand.ExecuteNonQuery();
-                dbConnections.Close();
-            }
-
-        }
         protected void Page_Load(object sender, EventArgs e)
         {
             if (Session["UserID"] == null)
